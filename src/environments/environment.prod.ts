d4e1e3de2ebc0785c759e5119d27e5613049a64e
Ajay R. Mall <mallajay19@gmail.com>
2020-01-12 12:17:08 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAKuBExBf670aAJyF4BB66o7BojVNQ3aRs",
-    authDomain: "zoom-image-sliderimage-ionic4.firebaseapp.com",
-    databaseURL: "https://zoom-image-sliderimage-ionic4.firebaseio.com",
-    projectId: "zoom-image-sliderimage-ionic4",
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     storageBucket: "",
-    messagingSenderId: "1043726835897",
-    appId: "1:1043726835897:web:7bec00cb019c1648"
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   }
 };
